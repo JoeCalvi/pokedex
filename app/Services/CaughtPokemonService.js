652f93c8b81bc0a4db0cd4a300d3367e5c679531
JoeCalvi <joecalvi@me.com>
2023-02-08 20:23:26 -0700

    added chance to not catch pokemon

--- app/Services/CaughtPokemonService.js
+++ app/Services/CaughtPokemonService.js
@@ -1,5 +1,6 @@
 import { appState } from "../AppState.js"
 import { CaughtPokemon } from "../Models/CaughtPokemon.js"
+import { Pop } from "../Utils/Pop.js"
 import { sandbox_api } from "./AxiosService.js"
 
 class CaughtPokemonService {
@@ -10,12 +11,18 @@ class CaughtPokemonService {
     }
 
     async catchPokemon() {
-        const res = await sandbox_api.post('/pokemon', appState.foundWildPokemon)
-        let newPokemon = new CaughtPokemon(res.data)
-        appState.caughtPokemon.push(newPokemon)
-        appState.emit('caughtPokemon')
-        appState.observedCaughtPokemon = newPokemon
-        console.log('[caught pokemon]', appState.caughtPokemon)
+        let roll = (Math.random() * 100)
+        if (roll >= 70) {
+            const res = await sandbox_api.post('/pokemon', appState.foundWildPokemon)
+            let newPokemon = new CaughtPokemon(res.data)
+            appState.caughtPokemon.push(newPokemon)
+            appState.emit('caughtPokemon')
+            appState.observedCaughtPokemon = newPokemon
+            console.log(roll, '[caught pokemon]', appState.caughtPokemon)
+        } else {
+            Pop.toast(`${appState.foundWildPokemon.name} resisted your attempt!`)
+            console.log(roll)
+        }
     }
 
 }
